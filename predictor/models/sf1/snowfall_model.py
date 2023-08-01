5bc81678d0509585a44e640042e35702b3820c59
IcePhoenix09 <vlasuk.roma04@gmail.com>
2023-08-01 15:03:04 +0300

    Add testing module
    Module using matplotlib to test how network performe.

--- predictor/models/sf1/snowfall_model.py
+++ predictor/models/sf1/snowfall_model.py
@@ -7,30 +7,39 @@ import os
 from .config_parser import get_network_data
 
 class Snowfall():
-    def __init__(self):
-        data = get_network_data(os.path.join(os.path.dirname(__file__), "config.json"))
-        relative_path = data["relative-path"]
-        self.NUM_OF_PREV_ITEMS = data["num-of-privious-item"]
+    def __init__(self, model_name=None):
+        self.model_name = model_name
 
-
+        if model_name == None:
+            data = get_network_data(os.path.join(os.path.dirname(__file__), "config.json"))
+            relative_path = data["relative-path"]
+        else:
+            relative_path = model_name
+        
         path_to_model = os.path.join(os.path.dirname(__file__), relative_path)
         self.model = keras.models.load_model(path_to_model)
 
+        self.NUM_OF_PREV_ITEMS = self.model.layers[0].input_shape[2]
 
-    def predict_next(self, data_set):
-        """Predict next value using model"""
+    def make_prediction(self, data_set):
+
+        data_set = np.reshape(data_set, (-1, 1)) # TODO posibility of repeat operation(when using testing)
 
         # min-max normalixation (inverse to (0, 1) range)
         scaler = MinMaxScaler(feature_range=(0, 1))
-        data_set = np.reshape(data_set, (-1, 1))
         data = scaler.fit_transform(data_set)
 
-        data = np.reshape(data, (1, 1, self.NUM_OF_PREV_ITEMS))
+        data = np.reshape(data, (-1, 1, self.NUM_OF_PREV_ITEMS))
 
         prediction = self.model.predict(np.array(data))
 
         # inverse from (0, 1) range to actual value
         prediction = scaler.inverse_transform(prediction)
+
+        return prediction
+
+    def predict_next(self, data_set):
+        prediction = self.make_prediction(data_set)
 
         return prediction[0][0]
     

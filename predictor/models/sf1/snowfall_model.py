3480018df2dc9d2169f0728a9bfc14b7d59838c9
IcePhoenix09 <vlasuk.roma04@gmail.com>
2023-10-22 20:06:53 +0300

    Fix predictor and requirements.txt
    I think some merge broke predictor a little, so I fixed a couple of lines here.
    To run the project I need influxdb-client library too.

--- predictor/models/sf1/snowfall_model.py
+++ predictor/models/sf1/snowfall_model.py
@@ -12,14 +12,14 @@ def parse_dataframe(dataframe):
 
 
 class Snowfall(ModelHandler):
-    def __init__(self, model_name='model_15m'):
+    def __init__(self, model_name='model_15m_50:1_c-c'):
         super().__init__()
         self.model_name = model_name
 
         path_to_model = os.path.join(self.PATH_TO_CONF, model_name)
         self.model = keras.models.load_model(path_to_model)
 
-        self.NUM_OF_PREV_ITEMS = self.model.layers[0].input_shape[2]
+        self.NUM_OF_PREV_ITEMS = self.model.layers[0].input_shape[1]
 
     def make_prediction(self, data_set):
         data_set = np.reshape(data_set, (-1, 1))
@@ -27,8 +27,6 @@ class Snowfall(ModelHandler):
         # min-max normalization (inverse to (0, 1) range)
         scaler = MinMaxScaler(feature_range=(0, 1))
         data = scaler.fit_transform(data_set)
-
-        data = np.reshape(data, (-1, 1, self.NUM_OF_PREV_ITEMS))
 
         prediction = self.model.predict(np.array(data))
 
